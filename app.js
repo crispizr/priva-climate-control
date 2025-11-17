d04f966e3de049a5ffc0d09d1d2f49447cc6a501
crispizr <dailafrancky@gmail.com>
2025-11-17 10:32:47 +0000

    Remove extra semicolons from CSV URL constants
    Signed-off-by: crispizr <dailafrancky@gmail.com>

--- app.js
+++ app.js
@@ -6,9 +6,9 @@
 
 const COMMAND_API_URL = 'https://script.google.com/macros/s/AKfycbwA53tJWrpVpd6WeoAA09FYVe63aFvwy-liD_rQgb2gr_HZ2bYHC1sKajJ4wzwshMC6aA/exec';
 
-const AGRICULTURE_CSV_URL =  "https://docs.google.com/spreadsheets/d/12x5LRuFBaKeAfkSxc53uR-6Q3Xcu-OxZt2plY0GZSko/export?format=csv&gid=2127989880";;
+const AGRICULTURE_CSV_URL = "https://docs.google.com/spreadsheets/d/12x5LRuFBaKeAfkSxc53uR-6Q3Xcu-OxZt2plY0GZSko/export?format=csv&gid=2127989880";
 
-const SECURITY_CSV_URL =  "https://docs.google.com/spreadsheets/d/12x5LRuFBaKeAfkSxc53uR-6Q3Xcu-OxZt2plY0GZSko/export?format=csv&gid=2127989880";;
+const SECURITY_CSV_URL = "https://docs.google.com/spreadsheets/d/12x5LRuFBaKeAfkSxc53uR-6Q3Xcu-OxZt2plY0GZSko/export?format=csv&gid=2127989880";
 
 const PROXY = 'https://api.allorigins.win/raw?url=';
 
