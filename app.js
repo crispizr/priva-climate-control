41379e65ff6d4bc2ff10ec57338ea53f9af7b532
crispizr <dailafrancky@gmail.com>
2025-12-10 10:49:22 +0000

    Increase camera refresh rate to 20 FPS
    b
    
    Signed-off-by: crispizr <dailafrancky@gmail.com>

--- app.js
+++ app.js
@@ -724,7 +724,7 @@ function startCameraAutoRefresh(id, ip) {
     } else {
       clearInterval(cameraRefreshIntervals[id]);
     }
-  }, 500); // 500ms = 2 FPS
+  }, 50); // 500ms = 2 FPS
 }
 
 // ===== GESTION ERREUR =====
