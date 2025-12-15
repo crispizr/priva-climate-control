31594ce949792be3eb0833cf08cbc590b5c4a8ac
crispizr <dailafrancky@gmail.com>
2025-12-15 09:54:44 +0000

    Decrease camera refresh interval
    Reduce camera refresh interval from 800ms to 300ms.
    
    Signed-off-by: crispizr <dailafrancky@gmail.com>

--- app.js
+++ app.js
@@ -724,7 +724,7 @@ function startCameraAutoRefresh(id, ip) {
     } else {
       clearInterval(cameraRefreshIntervals[id]);
     }
-  }, 800); // 500ms = 2 FPS
+  }, 300); // 500ms = 2 FPS
 }
 
 // ===== GESTION ERREUR =====
