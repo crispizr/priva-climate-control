a412d27310cc6cd3f117da252d46cdf2924c6c20
crispizr <dailafrancky@gmail.com>
2025-12-20 04:45:12 +0000

    Update CACHE_NAME to force cache refresh
    A1now
    
    Signed-off-by: crispizr <dailafrancky@gmail.com>

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * PRIVA Climate Control - Service Worker CORRIGÉ
  */
 
-const CACHE_NAME = 'priva-v6';  // ← Version changée pour forcer mise à jour
+const CACHE_NAME = 'priva-v=A1';  // ← Version changée pour forcer mise à jour
 const BASE_PATH = '/priva-climate-control';
 
 const FILES_TO_CACHE = [
