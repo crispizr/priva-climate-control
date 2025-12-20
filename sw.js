9bbefc828670dbc51bf0ab85456c1923b529c8dc
crispizr <dailafrancky@gmail.com>
2025-12-20 04:36:14 +0000

    Change CACHE_NAME from 'priva-v5' to 'priva-v6'
    Updated cache version to force update.
    
    Signed-off-by: crispizr <dailafrancky@gmail.com>

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * PRIVA Climate Control - Service Worker CORRIGÉ
  */
 
-const CACHE_NAME = 'priva-v5';  // ← Version changée pour forcer mise à jour
+const CACHE_NAME = 'priva-v6';  // ← Version changée pour forcer mise à jour
 const BASE_PATH = '/priva-climate-control';
 
 const FILES_TO_CACHE = [
