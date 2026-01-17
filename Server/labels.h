e6a6f5942ac21e3f7728662050e0912b35791405
crispizr <dailafrancky@gmail.com>
2026-01-17 16:02:54 +0000

    Add files via upload
    Signed-off-by: crispizr <dailafrancky@gmail.com>

---
+++ Server/labels.h
@@ -0,0 +1,14 @@
+// Labels - Caméra vs Microscope
+// Généré automatiquement
+
+#ifndef LABELS_H
+#define LABELS_H
+
+const int NUM_LABELS = 2;
+
+const char* labels[] = {
+  "Camera",
+  "Microscop"
+};
+
+#endif // LABELS_H
