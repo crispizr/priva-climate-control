cbc1e984881a2e188285600dcc833588fd9f562c
crispizr <dailafrancky@gmail.com>
2026-01-30 16:51:34 +0000

    Update sw.js
    Signed-off-by: crispizr <dailafrancky@gmail.com>

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * PRIVA Climate Control - Service Worker CORRIGÉ
  */
 
-const CACHE_NAME = 'priva-v=B3';  // ← Version changée pour forcer mise à jour
+const CACHE_NAME = 'priva-v=B2';  // ← Version changée pour forcer mise à jour
 const BASE_PATH = '/priva-climate-control';
 
 const FILES_TO_CACHE = [
