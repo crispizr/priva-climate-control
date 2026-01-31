28707adca3e6eafd07c937e4fb35f56f3c0776e6
crispizr <dailafrancky@gmail.com>
2026-01-31 23:06:42 +0000

    Update sw.js
    KG
    
    Signed-off-by: crispizr <dailafrancky@gmail.com>

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * PRIVA Climate Control - Service Worker CORRIGÉ
  */
 
-const CACHE_NAME = 'priva-v=A22';  // ← Version changée pour forcer mise à jour
+const CACHE_NAME = 'priva-v=A12';  // ← Version changée pour forcer mise à jour
 const BASE_PATH = '/priva-climate-control';
 
 const FILES_TO_CACHE = [
